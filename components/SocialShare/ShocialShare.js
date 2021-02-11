b054dbd598492f6ddc2c98306c05373136ba362a
mollerjorge <jmoller2106@gmail.com>
2021-02-11 12:08:03 -0300

    Fix social share

--- components/SocialShare/ShocialShare.js
+++ components/SocialShare/ShocialShare.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { useRouter } from 'next/router'
 
 const ShocialShare = ({ postTitle, postUrl }) => {
-  return (
+  //cwuniverse-app.vercel.app/
+  https: return (
     <div className="container flex">
       <a
-        href={`https://twitter.com/intent/tweet?text=${postTitle}%20https://clockwork.app${postUrl}`}
+        href={`https://twitter.com/intent/tweet?text=${postTitle}%20https://cwuniverse-app.vercel.app/${postUrl}`}
         target="_blank"
         rel="noreferrer"
         className="py-2 px-7 border-2 border-blue rounded text-blue font-lato antialiased transform hover:-translate-y-1 transition-all hover:shadow-lg"
@@ -12,7 +14,7 @@ const ShocialShare = ({ postTitle, postUrl }) => {
         twitter
       </a>
       <a
-        href={`https://www.facebook.com/sharer/sharer.php?u=https://clockwork.app/blog/${postUrl}`}
+        href={`https://www.facebook.com/sharer/sharer.php?u=https://cwuniverse-app.vercel.app/${postUrl}`}
         target="_blank"
         rel="noreferrer"
         className="ml-5 py-2 px-7 border-2 border-blue rounded text-blue font-lato antialiased transform hover:-translate-y-1 transition-all hover:shadow-lg"
@@ -20,7 +22,7 @@ const ShocialShare = ({ postTitle, postUrl }) => {
         facebook
       </a>
       <a
-        href={`http://www.linkedin.com/shareArticle?mini=true&url=https://clockwork.app/blog/${postUrl}/&title=${postTitle}`}
+        href={`http://www.linkedin.com/shareArticle?mini=true&url=https://cwuniverse-app.vercel.app/${postUrl}&title=The%20Building%20Blocks%20of%20Successful%20Investor%20Reporting`}
         target="_blank"
         rel="noreferrer"
         className="ml-5 py-2 px-7 border-2 border-blue rounded text-blue font-lato antialiased transform hover:-translate-y-1 transition-all hover:shadow-lg"
