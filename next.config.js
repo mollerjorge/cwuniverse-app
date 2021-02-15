ceb8ace441e46fdd730432af80c4958b91f03232
mollerjorge <jmoller2106@gmail.com>
2021-02-15 15:03:06 -0300

    Remove optimize fonts

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 const withMDX = require('@next/mdx')()
 
 module.exports = withMDX({
-  experimental: {
-    optimizeFonts: true,
-  },
   pageExtensions: ['js', 'jsx', 'md', 'mdx'],
   webpack(config) {
     config.module.rules.push({
