25ada4accb085c2ce21d4d59eff805965d4a16f6
mollerjorge <jmoller2106@gmail.com>
2021-04-14 17:06:44 -0300

    Add public url to socialshare

--- components/SocialShare/ShocialShare.js
+++ components/SocialShare/ShocialShare.js
@@ -9,7 +9,7 @@ const ShocialShare = ({ postTitle }) => {
   return (
     <div className="flex justify-between md:justify-start">
       <a
-        href={`https://twitter.com/intent/tweet?text=${postTitle}%20https://cwuniverse-app.vercel.app${router.pathname}`}
+        href={`https://twitter.com/intent/tweet?text=${postTitle}%20${process.env.NEXT_PUBLIC_HOST}${router.pathname}`}
         target="_blank"
         rel="noreferrer"
         className="py-2 px-4 md:px-7 border-2 border-blue flex justify-center flex-col items-center md:flex-row rounded text-blue font-lato antialiased transform hover:-translate-y-1 transition-all hover:shadow-lg"
@@ -18,7 +18,7 @@ const ShocialShare = ({ postTitle }) => {
         twitter
       </a>
       <a
-        href={`https://www.facebook.com/sharer/sharer.php?u=https://cwuniverse-app.vercel.app${router.pathname}`}
+        href={`https://www.facebook.com/sharer/sharer.php?u=${process.env.NEXT_PUBLIC_HOST}${router.pathname}`}
         target="_blank"
         rel="noreferrer"
         className="ml-2 md:ml-5 py-2 px-4 md:px-7 border-2 border-blue flex justify-center flex-col items-center md:flex-row rounded text-blue font-lato antialiased transform hover:-translate-y-1 transition-all hover:shadow-lg"
@@ -27,7 +27,7 @@ const ShocialShare = ({ postTitle }) => {
         facebook
       </a>
       <a
-        href={`http://www.linkedin.com/shareArticle?mini=true&url=https://cwuniverse-app.vercel.app${router.pathname}&title=The%20Building%20Blocks%20of%20Successful%20Investor%20Reporting`}
+        href={`http://www.linkedin.com/shareArticle?mini=true&url=${process.env.NEXT_PUBLIC_HOST}${router.pathname}&title=The%20Building%20Blocks%20of%20Successful%20Investor%20Reporting`}
         target="_blank"
         rel="noreferrer"
         className="ml-2 md:ml-5 py-2 px-4 md:px-7 border-2 border-blue flex justify-center flex-col items-center md:flex-row rounded text-blue font-lato antialiased transform hover:-translate-y-1 transition-all hover:shadow-lg"
