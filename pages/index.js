609b5bf68abc997ebe4ec329f38d079a6566c560
Matthew Reed <mr.matthew.reed@gmail.com>
2021-06-22 12:14:57 -0400

    Image Optim Batch Job

--- pages/index.js
+++ pages/index.js
@@ -49,7 +49,7 @@ export default function Home() {
             <div className="mt-20  md:mt-40 lg:mt-12 lg:flex lg:justify-between lg:items-center">
               <div className="w-full lg:w-1/2">
                 <h1 className="text-big font-raleway font-normal text-gray-700 max-w-sm lg:text-60 lg:max-w-2xl">
-                  The evolution of portfolio operations.
+                  The evolution of portfolio operations
                 </h1>
                 <p className="font-lato text-xl mt-6  text-black font-light lg:max-w-lg antialiased">
                   Clockwork Universe powers the actionable <br />
